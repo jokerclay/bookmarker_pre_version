dc66f80cc7a9323e887ad4461eeaeee7b6e58662
jokerclay <2079209547@qq.com>
2021-09-08 22:15:55 +0800

    added search autofoucs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let modalBtn = document.getElementById('searchBtn')
 let modalBg = document.querySelector('.modal-bg')
 let modalClose = document.querySelector('.modal-close')
 let globalKeyBorad = document.body
+let searchFiled = $('input[name="search"]')
 
 // 响应鼠标 点击时 弹窗
 modalBtn.addEventListener('click', () => {
@@ -17,7 +18,9 @@ globalKeyBorad.addEventListener('keydown', e => {
 
     console.log(e)
     if (e.key == "S") {
-        modalBg.classList.add('bg-active')
+        modalBg.classList.add('bg-active');
+        // 当 搜索框弹出时，使 input 自动 fouce
+        setTimeout(() => { searchFiled.focus() }, 255)
     }
 })
 globalKeyBorad.addEventListener('keydown', e => {
@@ -26,4 +29,4 @@ globalKeyBorad.addEventListener('keydown', e => {
     if (e.key == "Escape") {
         modalBg.classList.remove('bg-active')
     }
-})
+});
